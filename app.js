b0e5d9e186b691e7face621a0054639d3eb0bf19
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-04-19 15:13:31 -0700

    front page and character creation functionality complete

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
-import { createUser } from "./local-storage-utils";
+import { createUser, setUser } from "./local-storage-utils.js";
 
-const attributeArray = document.querySelectorAll('.attribute-inputs');
+const attributeInputs = document.querySelectorAll('.attribute-inputs');
 const totalAttributePoints = document.querySelector('#available-attribute-points');
 
 const form = document.querySelector('form');
 
-const intelligence = Number(attributeArray[0].value);
-const strength = Number(attributeArray[1].value);
-const marksmanship = Number(attributeArray[2].value);
-const charisma = Number(attributeArray[3].value);
-const luck = Number(attributeArray[4].value);
 
-console.log(attributeArray)
 form.addEventListener('change', (e) => {
     e.preventDefault();
-    const intelligence = Number(attributeArray[0].value);
-    const strength = Number(attributeArray[1].value);
-    const marksmanship = Number(attributeArray[2].value);
-    const charisma = Number(attributeArray[3].value);
-    const luck = Number(attributeArray[4].value);
+    const intelligence = Number(attributeInputs[0].value);
+    const strength = Number(attributeInputs[1].value);
+    const marksmanship = Number(attributeInputs[2].value);
+    const charisma = Number(attributeInputs[3].value);
+    const luck = Number(attributeInputs[4].value);
+
     let attributeTotal = strength + intelligence + marksmanship + charisma + luck;
     let totalPoints = 8 - attributeTotal;
 
-    for (let attribute of attributeArray) {
+    for (let attribute of attributeInputs) {
         attribute.max = totalPoints + attribute.value;
     }
 
     totalAttributePoints.textContent = totalPoints;
 
+    const attributeArray = [intelligence, strength, marksmanship, charisma, luck]
+
+    return attributeArray;
 })
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    createUser()
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const tagline = formData.get('tagline');
+    const attributeArray = [
+        formData.get('intelligence'), 
+        formData.get('strength'),
+        formData.get('marksmanship'),
+        formData.get('charisma'),
+        formData.get('luck')
+    ]
+
+    const user = createUser(name, tagline, attributeArray);
+    setUser(user);
+    window.location = './map'
 })
