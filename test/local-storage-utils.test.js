26b814c1382ec44340c9e4b958cd56804efa3823
chase-abbott <cabbott93@gmail.com>
2021-04-19 10:25:13 -0700

    TDD local storage utils and user object

---
+++ test/local-storage-utils.test.js
@@ -0,0 +1,42 @@
+import { setUser, getUser } from '../local-storage-utils.js';
+
+const test = QUnit.test;
+
+test('Test setUser function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const user = {
+        age: 12,
+    }
+
+    setUser(user);
+
+    const expected = user;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = JSON.parse(localStorage.getItem('USER'));
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, expected);
+});
+
+
+test('Test getUser function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const user = {
+        age: 12,
+    }
+
+    setUser(user);
+
+    const expected = user;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getUser(user);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, expected);
+});
