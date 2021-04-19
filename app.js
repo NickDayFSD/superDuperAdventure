8d004d4fc3a908d9ad4496d209599fd8af9fa12d
chase-abbott <cabbott93@gmail.com>
2021-04-19 12:03:56 -0700

    attribute input logic

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const sliderArray = document.querySelectorAll('.slider-inputs');
+const spanArray = document.querySelectorAll('.input-span');
+const totalAttributePoints = document.querySelector('#available-attribute-points');
 
 const form = document.querySelector('form');
 
-console.log(sliderArray);
 
-for (let slider of sliderArray) {
-    const current = slider.value;
-    console.log(current)
-}
-// on mousemove
+console.log(sliderArray)
 form.addEventListener('change', (e) => {
     e.preventDefault();
+    const cunning = Number(sliderArray[0].value);
+    const strength = Number(sliderArray[1].value);
+    const marksmanship = Number(sliderArray[2].value);
+    const charisma = Number(sliderArray[3].value);
+    let attributeTotal = strength + cunning + marksmanship + charisma;
+    let totalPoints = 10 - attributeTotal;
 
-    const formData = new FormData(form);
-    const attributesData =
-        console.log(e.target.value);
+    for (let slider of sliderArray) {
+        let nextSibling = slider.nextElementSibling;
+        nextSibling.textContent = slider.value;
+        slider.max = totalPoints;
+    }
+
+    totalAttributePoints.textContent = totalPoints;
+
 })
 
 form.addEventListener('submit', (e) => {
