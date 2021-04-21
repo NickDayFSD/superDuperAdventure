24417a01a6cfaaf5d1a33e095e8562f343a2cc3b
chase-abbott <cabbott93@gmail.com>
2021-04-21 15:18:37 -0700

    add render header function

--- results/results.js
+++ results/results.js
@@ -1,10 +1,16 @@
 import { getUser, areQuestsCompleted } from '../local-storage-utils.js';
+import { renderHeader } from '../render-user-stats.js';
 
+const body = document.querySelector('body');
 const user = getUser();
 const canvas = document.querySelector('#main-canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 1200;
 canvas.height = 900;
+
+const header = renderHeader(user);
+
+body.append(header);
 
 
 if (areQuestsCompleted(user)) {
@@ -44,7 +50,7 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
 //         } 
 //     }
 // }
-    
+
 
 
 function animate() {
@@ -61,7 +67,7 @@ function animate() {
         player.width,
         player.height
     );
-    
+
     movePlayer();
     requestAnimationFrame(animate);
     // intersectRect();
