1b4f77a077b07fe3175f55ae5210a7fe43abf3e2
chase-abbott <cabbott93@gmail.com>
2021-04-22 11:55:43 -0700

    add dead results page

---
+++ results/dead.js
@@ -0,0 +1,37 @@
+import { findHighestStat, getUser } from '../local-storage-utils.js';
+
+const user = getUser();
+const retryButton = document.querySelector('#retry-button');
+const pResults = document.querySelector('#results-p');
+
+retryButton.addEventListener('click', () => {
+    localStorage.clear();
+    window.location = '../';
+});
+
+let userMorality;
+
+if (user.morality < -1) {
+    userMorality = 'an Evil';
+} else if (user.morality > 1) {
+    userMorality = 'a Good';
+} else {
+    userMorality = 'an Average';
+}
+
+let userFriends;
+
+if (user.friends.length === 1) {
+    userFriends = `${user.friends.length} friend`;
+} else {
+    userFriends = `${user.friends.length} friends`;
+}
+
+const userCredits = user.credits;
+
+// console.log(findHighestStat(user))
+// const highestSkill = findHighestStat(user);
+
+
+pResults.textContent = `You died ${userMorality} person. You left ${userCredits} credits behind for whoever finds your body and died with ${userFriends}. `;
+//Your ${highestSkill} couldn't save you this time
